e85edead3cb3ee5d05116eb720cc511a899d0a0f
Sebas <sebastian.v.a@riseup.net>
2024-07-29 16:03:35 +0200

    chore: Update git clone URL in commit.sh

--- commit.sh
+++ commit.sh
@@ -1,25 +1,44 @@
-AZUREPAT=$AZUREPAT
-AZUSERNAME=$AZUSERNAME
-AZUSER_EMAIL=$AZUSER_EMAIL
-AZORG=$AZORG
-git clone https://github.com/dadapunk/testAzureDevops.git
+#!/bin/bash
 
-cd testAzureDevops
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Set required environment variables
+AZUREPAT=${AZUREPAT:?Need to set AZUREPAT}
+AZUSERNAME=${AZUSERNAME:?Need to set AZUSERNAME}
+AZUSER_EMAIL=${AZUSER_EMAIL:?Need to set AZUSER_EMAIL}
+AZORG=${AZORG:?Need to set AZORG}
+
+# Set repository and folder names
+GITHUB_REPO="https://github.com/dadapunk/testAzureDevops.git"
+GITHUB_FOLDER="testAzureDevops"
+AZURE_FOLDER="TestGitSync"
+AZURE_REPO="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/$AZURE_FOLDER/_git/$AZURE_FOLDER"
+
+# Clone the GitHub repository
+git clone $GITHUB_REPO
+
+# Navigate to the cloned repository and remove the .git directory
+cd $GITHUB_FOLDER
 rm -rf .git
 
+# Navigate back to the parent directory
 cd ..
 
-GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/TestGitSync/_git/TestGitSync"
-git clone $GIT_CMD_REPOSITORY
+# Clone the Azure DevOps repository
+git clone $AZURE_REPO
 
-cp -r testAzureDevops/* TestGitSync/
+# Copy files from the GitHub repository to the Azure DevOps repository
+cp -r $GITHUB_FOLDER/* $AZURE_FOLDER/
 
-cd TestGitSync
+# Navigate to the Azure DevOps repository
+cd $AZURE_FOLDER
 
-git config --global user.email "$AZUSER_EMAIL"
-git config --global user.name "$AZUSERNAME"
+# Configure git user details locally
+git config user.email "$AZUSER_EMAIL"
+git config user.name "$AZUSERNAME"
 
+# Add, commit, and push changes
 git add .
 git commit -m "sync from git to azure"
-
 git push
